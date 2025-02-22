9d17f4dd10a79d035632b1aa5a4e2cea9395aa94
Sirius)(Black <Sirius_Black9999@hotmail.com>
2025-02-22 10:44:55 +0100

    initial commit

---
+++ src/main/java/com/quadexercise/quad/QuadApplication.java
@@ -0,0 +1,14 @@
+package com.quadexercise.quad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuadApplication.class, args);
+        
+    }
+
+}
